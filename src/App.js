5732f439f6b41be4eda41571e1ec27538b1ab72b
Arjen <arjenmeer@hotmail.nl>
2020-11-27 14:26:12 +0100

    Adding first styling and  non func login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './GlobalStyle.scss';
+import Home from './home/Home';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Login from './login/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH HELP
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          WiPP
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+
+  render() {
+    return (
+      <Router className="router">
+        <nav>
+          <ul className="router-list">
+            <li><Link to={'/home'}>Home</Link></li>
+            <li><Link to={'/userGames'}>Games</Link></li>
+            <li><Link to={'/joinGame'}>Find game</Link></li>
+            <li className="login-button"><Link to={'/login'}>Login</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/register' component={Home}/>
+
+          <Route exact path='/userGames' component={Home}/>
+          <Route exact path='/userGames/create' component={Home}/>
+
+          <Route exact path='/joinGame' component={Home}/>
+          <Route exact path='/lobby' component={Home}/>
+          <Route exact path='/game' component={Home}/>
+
+          <Route exact path='/home' component={Home}/>
+          <Route path='/' component={Home}/>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/login/Login.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import './LoginStyle.scss';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: "",
+            password: "",
+        }
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.toRegiter = this.toRegiter.bind(this);
+    }
+
+    handleNameChange(event){
+        this.setState({username: event.target.value});
+    }
+
+    handlePasswordChange(event){
+        this.setState({password: event.target.value});
+    }
+
+    login() {
+        if (this.state.username === "" || this.state.password === "") {
+            this.setState({loginError: "Please fill in all fields"})
+            return;
+        }
+
+        var credentials = { username:this.state.username, password:this.state.password };
+        
+        console.log(credentials);
+    }
+
+    toRegiter(){
+        this.props.history.push("/register");
+    }
+
+    render() {
+        return (
+            <div className="center">
+                <div className="login-fields">
+                    <b className="login-title">Login</b>
+
+                    <label htmlFor="username">Username:</label>
+                    <input id="username" className="login-username-input" type="text" placeholder="Username" value={this.state.username} onChange={this.handleNameChange}></input>
+
+                    <label htmlFor="password">Password:</label>
+                    <input id="password" className="login-username-input" type="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}></input>
+
+                    <button className="login-submit-button" onClick={() => this.login()}>Login</button>
+                    <b className="login-error">{this.state.loginError}</b>
+
+                    <div className="to-register-link" onClick={this.toRegiter}>Don't have an account yet? Register here.</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
